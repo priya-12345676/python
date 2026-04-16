ae2d6acbee927bdf68e1a973ff3ad6fe95f5fd17
priya-12345676 <pssrivastav855@gmail.com>
2026-04-16 23:44:46 +0530

    Add files via upload

---
+++ datatypes.py.py
@@ -0,0 +1,46 @@
+#Number=0,1,2,3,34,4.5,-45
+#String="dsskjbjhfjd122329-=(#$@%^&)"
+#Boolean=True,False
+#Sequence ={"Priya", 23,"jaipur"}
+#Mapping (Dict) ={"name": "Liza",
+"number of shoes":5,
+"Add":"jaipur"
+}
+#comments
+
+#int
+'''Integer means every + or -ve non decimal number including 0.'''
+
+#float
+#Any + or -ve decimal number are considered a float.
+
+#bool
+#True,False
+
+#4 Rules 
+#It should not start with a number
+#Only_are allowed in a variable name
+#Variable names are case sensitive
+#It should not be a keyword of the language
+
+#Keyword
+#These are the reserved names by the language.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
