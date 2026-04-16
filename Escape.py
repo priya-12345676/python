c9b345b91f44b4f12062e269e3ccd43615406d13
priya-12345676 <pssrivastav855@gmail.com>
2026-04-16 23:45:28 +0530

    Add files via upload

---
+++ Escape.py
@@ -0,0 +1,16 @@
+#print("the\npython")
+#print("I am\t tap")
+#print("she\her")
+#print("they\\them")
+#print("they\\them") # this is a comment
+print("hello\rworld")
+print("hello\b world")
+print("i am form \f up")
+print("\a")
+print("i live in \INDIA")
+
+
+
+
+
+
