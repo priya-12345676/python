cb784b7cc4115222d0e00b991932b3d4dda1a024
priya-12345676 <pssrivastav855@gmail.com>
2026-04-18 22:58:43 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,115 @@
+#print( "starting the program" )
+#if 34 > 10 :
+#print("number is greater")
+#print("program end here")
+
+#if 9 > 10 :
+ #print("number is greater.")
+#else:
+ #print("number is small.")
+
+#if "A" and "" or "C" :
+ #print("number is greater.")
+#else:
+ #print("number is small.")
+
+#if 5 // 2 :
+ #print("if is running")
+#else:
+ #print("else is running")
+
+
+#Take a input from username and if it's admin then print you can add people
+#else you can not add people.
+
+#username = input("Enter Your Username:")
+#if username == "admin":
+#   print("You can add people")
+#else:
+#   print("You can not add people")
+
+
+# 5 Vowel or Consonant Checker
+# This program asks the user to input a single letter and checks if it's a 
+char = input("Enter only 1 charactar:")
+
+if char == "a":
+   print("vowel")
+elif char == "e":
+   print("vowel")
+elif char == "i":
+   print("vowel")
+elif char == "o":
+   print("vowel")
+elif char == "u":
+   print("vowel")
+else:
+   print("consonent")
+
+#in
+
+#if char in "aeiouAEIOU":
+ #     print("vowel")
+#else:
+ #    print("consonant")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
