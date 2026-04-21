c8269500e372f61d3297041b7207be54d329ba9c
priya-12345676 <pssrivastav855@gmail.com>
2026-04-21 23:16:07 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,134 @@
+#what is  dictionary?
+#A dictionary is an unordered collection of items. Each item is stored as a  key-value pair.
+#Unordered: Historically,dictonnaries were unordered.From Python 3.7 onwords.
+#Mutable: You can add , remove and modify key-value pairs after the dictionary.
+#values can be anything 
+#values can be of anydata type and can be duplicates.
+#keys: keys are the unique data in a dict and any immutable datatype .
+#Mapping:Dictionary are often referred of as mapping.
+#we use the opertor to separeate the  key and value.
+
+#left side=  key
+#Right side= value
+#dictionary ko black mai only brakit sy likha ja skta hai={}
+
+
+
+#a =  {"name": "Alice", "age":"24", "city":"berlin"}
+#print(a)
+
+#An empty dictionary
+#empty_dict = {} #dict()
+#print(empty_dict)
+
+#A dictionary  with  string keys and various values
+#person = {"name":"Alice","age":"25","city":"new york",}
+
+#person = {
+#     "name":"Alice",
+#     "age":"25",
+#     "city":"new york"
+#}
+
+#data ={  
+#        1:"Alice",
+#        2:30,
+#        3:"new york",
+#        3:"test"
+#       }
+#print(data)
+
+## A dictionary with mixed  key types (valid because all  are immutable and unique)
+#mixed_keys_dict =  {
+#         "name":"mumbai",
+#         1: "one",
+#         3.14:"pi",
+#        (1,2):"tuple key"
+#                  }
+#print(mixed_keys_dict)
+
+#Accessing values
+#a = {"name":"Berlin","age":30,"city":"Berlin"}
+#print(a["name"])
+
+#a={
+#  "name":"Alice",
+#  "age":30,
+#  "city":"New york",
+#  "data1":{
+#       "level":"Advanced",
+#       "data2": [1,2,3,4,5,{"data3":"dummy_value"}]
+#        }
+#  }
+#print(a["data1"])
+#output={level":"Advanced",
+#       "data2": [1,2,3,4,5,{"data3":"dummy_value"}]}
+
+#print(a["data1"]["level"])
+#output=Advanced
+
+#print(a["data1"]["data2"][-1]["data3"])
+#output=dummy_value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
