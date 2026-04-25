64b5b0de1883ebe36ddd94392d1407da51214798
priya-12345676 <pssrivastav855@gmail.com>
2026-04-25 22:57:41 +0530

    Add files via upload

---
+++ fstring.py
@@ -0,0 +1,44 @@
+#Formatted String/ F String
+
+#name = "priya"
+#age = 24
+#city = "jaipur"
+
+#My name is priya and age 24 and city is jaipur.
+
+#print("my name is",name,"and age",age,"and city is" ,city,)
+
+#print(f"my name is {name} and age is {age} and city is {city}.")
+
+#Indexind
+#my_string = "Hello"
+#print(my_string[0])
+
+#a="jaipur"
+#counter=0
+#while counter < 5:
+#      print(a[counter])
+#      counter += 1
+
+#alg alg word aa jayenge
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
