180c2b7de0e822d328d89555ddb2f361f1e5fd54
priya-12345676 <pssrivastav855@gmail.com>
2026-05-06 22:46:59 +0530

    Add files via upload

---
+++ match.py
@@ -0,0 +1,68 @@
+#Membership Operators
+
+#Test membership in sequences like lists, string:
+#in True present "a" in "cat"
+#not in True if not present "x" not in "cat"
+
+#a = "i live in india."
+
+#print("india" in a)
+
+
+#Identity Operaors
+# Check it two variables refer to the same object:
+#is same object x is y
+#is not  not the same object x is not y
+
+#a = 25
+#b = 25.0
+#print(a is b)
+
+#Match case
+
+char = input("Enter only 1 charactar:")
+    
+match char:
+    case"a":
+          print("vowel")
+    case"e":
+          print("vowel")
+    case"i":
+          print("vowel")
+    case"o":
+          print("vowel")
+    case"u":
+          print("vowel")
+    case _:
+         print("consonent") 
+
+
+
+while'false':
+print("hello world1")
+print("hello world2")
+print("hello world3")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
